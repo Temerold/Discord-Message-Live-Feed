fddca227829fa79a6ab639952b2cb0e8345fbbfc
Temerold <67090612+Temerold@users.noreply.github.com>
2021-06-04 21:28:31 +0200

    File for authorization keys and tokens

---
+++ auth.py
@@ -0,0 +1,7 @@
+consumer_key        = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+consumer_secret     = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+access_token        = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+access_token_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+discord_token       = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# In this file you add the Twitter and and Discord APIs' authorization keys and tokens
