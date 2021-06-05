5607e68a76b9baad2198e117bdefbae39c07fac7
Temerold <67090612+Temerold@users.noreply.github.com>
2021-06-05 17:43:57 +0200

    Minor bug fixed and formatting
    If a message contained an attachment and more than 280 characters, then (before the fix) the last spliced-up tweet tweeted will have no text, only the image. Now that accidental last tweet isn't possible.

--- MLF.py
+++ MLF.py
@@ -1,4 +1,3 @@
-from urllib import parse
 import tweepy
 import discord
 from discord.ext import commands
@@ -6,21 +5,18 @@ from datetime import datetime
 import pytz
 import io
 from auth import *
-from alive import keep_alive
 
 
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix=".")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 	# Auth from auth.py
 
 image = False
 
 def tweet(message, im=False):
-	global image
-
 	if im: api.update_with_media("image.png", str(message)) # Sends message and attachemnt as tweet
 	else:
 		try: api.update_status(str(message)) # Sends message as tweet
@@ -30,13 +26,12 @@ def tweet(message, im=False):
 		f.write(f"{message}\n") # Logs message to file (tweets.txt)
 	print(f"Tweetade: '{message}'\n") # Logs message to console
 	
-	image = False
 
 @client.event
 async def on_message(message):
 	global image
 
-	date = datetime.now(pytz.timezone('Etc/GMT-2')).strftime("%Y-%m-%d %H:%M:%S") # Time read by bot, in GMT-2 time zone.
+	date = datetime.now(pytz.timezone('Etc/GMT-2')).strftime("%Y-%m-%d %H:%M:%S") # Time read by bot, in GMT-2 time zone
 
 	mes = f"[{date}]\n*{str(message.author.roles[-1])}* {message.author}: ❝{message.content}❞"
 		# [Time read by bot]
@@ -46,18 +41,18 @@ async def on_message(message):
 		await attachment.save("image.png")
 		image = True
 
-	link_length = 51 # len(twitter.com/TemeroldFeed/status/xxxxxxxxxxxxxxxxxxx) = 51
+	link_length = 51 # len("twitter.com/TemeroldFeed/status/xxxxxxxxxxxxxxxxxxx") = 51
 	max_length = 280 - link_length # Max length of actual message, as the referred tweet takes link_length extra chars.
 
 
-	if len(mes) >= max_length: # The message (mes) doesn't fit in one tweet.
-		parts = [] # Empty to-be spliced up message (mes) array.
+	if len(mes) >= max_length: # The message (mes) doesn't fit in one tweet
+		parts = [] # Empty to-be spliced up message (mes) array
 		while mes != "":
 			# Appends message (mes) to parts array, until it's entirely spliced up (with a length of max_length) into the array
 			parts.append(mes[:max_length])
 			mes = mes[max_length:]
 			
-		tweet(parts[0], image) # Tweets first part without referring to latest item in timeline, and 
+		tweet(parts[0], image) # Tweets first part without referring to latest item in timeline, including potential image
 		del parts[0] # Deletes already tweeted part of message (mes) (first part, with no reffering)
 
 		for i in parts:
@@ -65,7 +60,8 @@ async def on_message(message):
 			timeline = api.home_timeline()
 			tweet(f"{i} https://twitter.com/TemeroldFeed/status/{timeline[0].id}", image)
 
-	tweet(mes, image) # Calls tweet function, including potential image
+	else: tweet(mes, image) # Calls tweet function, including potential image
 
-keep_alive() # Runs server ping-receiver
-client.run(discord_token) # Runs Discord bot client, indefinitely
+	image = False
+
+client.run(discord_token) # Runs Discord bot client
